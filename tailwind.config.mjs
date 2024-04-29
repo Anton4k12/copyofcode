7f1a195b6cbf0c09419f6c6487e40dceaca8e63a
Anton4k12 <neanton2008@gmail.com>
2024-04-29 20:17:25 +0300

    add style for cards

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontSize: {
+        // heading: ["64px", "1.1em"],
+        // "card-heading": ["52px", "1.1em"],
+        heading: [
+          "64px",
+          {
+            lineHeight: "1.1em",
+            fontWeight: 600,
+          },
+        ],
+        "card-heading": [
+          "52px",
+          {
+            lineHeight: "1.1em",
+            fontWeight: 600,
+          },
+        ],
+        "card-description": ["18px", "1.4em"],
+      },
+    },
+  },
+  plugins: [],
+};
+
+// {'2xl': ['1.5rem', {
+// 	lineHeight: '2rem',
+// 	letterSpacing: '-0.01em',
+// 	fontWeight: '500',}]
