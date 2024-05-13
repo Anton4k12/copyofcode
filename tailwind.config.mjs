29cff6f6f6e6fb5f5eba34613ef6428b106cc6ee
Anton4k12 <neanton2008@gmail.com>
2024-05-13 18:48:35 +0300

    updated update

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,10 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    container: {
+      center: true,
+      padding: "1rem",
+    },
     extend: {
       fontSize: {
         // heading: ["64px", "1.1em"],
